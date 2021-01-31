4d9d281d1c86c2ef19cba2114f71280f23286c52
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-01-31 10:31:55 +0000

    commit on 'main'

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -17,8 +17,10 @@ def stations_by_distance(stations, p):
     for station in stations:
         stations_by_distance.append((station.name, haversine(station.coord, p)))
     
-    stations_by_distance = utils.sorted_by_key(stations_by_distance)
+    stations_by_distance = sorted_by_key(stations_by_distance, 1)
 
     return stations_by_distance
+
+
 
 
