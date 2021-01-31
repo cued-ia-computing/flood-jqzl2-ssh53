44fd66cb98a799d33aa57cd21b314f73e56aa1b7
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-01-31 21:37:18 +0000

    commit on 'main'

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -38,3 +38,14 @@ class MonitoringStation:
         d += "   river:         {}\n".format(self.river)
         d += "   typical range: {}".format(self.typical_range)
         return d
+    
+    def typical_range_consistent(self):
+        if self.typical_range == None:
+            return False
+        
+        else:
+            if self.typical_range[0] < self.typical_range[1]:
+                return True
+            
+            elif self.typical_range [0] > self.typical_range[1]:
+                return False
