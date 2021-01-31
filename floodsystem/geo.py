837f461e71d7fef633833267596b3b5b3146ac6e
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-01-31 10:13:20 +0000

    commit on 'main'

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,18 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+
+# Task 1B
+def stations_by_distance(stations, p):
+    
+    stations_by_distance = []
+    
+    for station in stations:
+        stations_by_distance.append((station.name, haversine(station.coord, p)))
+    
+    stations_by_distance = utils.sorted_by_key(stations_by_distance)
+
+    return stations_by_distance
+
+
