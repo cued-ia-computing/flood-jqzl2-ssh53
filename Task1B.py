9156afc89f8050db52c21fec877fae554a2fd5ec
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-02-10 14:34:43 +0000

    commit on 'main'

--- Task1B.py
+++ Task1B.py
@@ -6,8 +6,14 @@ def run():
     stations = build_station_list()
     p = (52.2053, 0.1218)
     x = stations_by_distance(stations, p)
-    print(x[:10])
-    print(x[-10:])
+    y = []
+    for i in x:
+        y.append((i[0].name, i[0].town, i[1]))
+    
+    print(y[:10])
+    print(y[-10:])
+
+
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
