eda9733d8ad04438d1c8170b60f2da4e6785617c
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-02-22 09:36:37 +0000

    2B

---
+++ test_flood.py
@@ -0,0 +1,16 @@
+import numpy as np 
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+def test_stations_level_over_threshold():
+    tol = np.random.randn()
+    stations = build_station_list()
+    update_water_levels(stations)
+    x = stations_level_over_threshold(stations, tol)
+    for i in range(0, len(x)):
+        assert x[i][1] > tol
+        if i == 0:
+            pass
+        elif i > 0:
+            assert x[i][1] < x[i - 1][1]
+
+    
