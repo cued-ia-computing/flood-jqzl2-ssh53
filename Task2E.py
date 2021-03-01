4256c3177094ea428db72f783f4a68349e0655c2
Jeffrey Lu <jqzl2@cam.ac.uk>
2021-03-01 21:18:29 +0000

    2E

--- Task2E.py
+++ Task2E.py
@@ -2,8 +2,17 @@ import datetime
 from floodsystem.plot import plot_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
 
 def run():
     stations = build_station_list()
-    
+    stations = stations_highest_rel_level(stations, 5)
+    for station in stations:
+        dt = 10
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
 
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,13 +3,16 @@ import datetime
 
 def plot_water_levels(station, dates, levels):
     plt.plot(dates, levels)
+    plt.plot(dates, station.typical_range[0])
+    plt.plot(dates, station.typical_range[1])
+    
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
     plt.title(station.name)
+    
     plt.tight_layout()
-    plt.plot(dates, station.typical_range[0])
-    plt.plot(dates, station.typical_range[1])
+
     plt.show()
     
 
